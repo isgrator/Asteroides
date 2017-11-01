cb3561fa0054cd95ef21d926489298b5d28e2266
Isabel <isgrator@gmail.com>
2017-11-01 15:35:31 +0100

    Ej: Implm. de una caja Acerca de

---
+++ app/src/main/java/org/imgt/asteroides/AcercaDeActivity.java
@@ -0,0 +1,14 @@
+package org.imgt.asteroides;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class AcercaDeActivity extends Activity {
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acercade);
+    }
+}
