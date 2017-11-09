7bf620fbf55ea16b38e5f66f6dd8cf2c7c2b0290
Isabel <isgrator@gmail.com>
2017-11-09 01:01:41 +0100

    Ej: La clase VistaJuego (4.3.2)

---
+++ app/src/main/java/org/imgt/asteroides/VistaJuego.java
@@ -0,0 +1,52 @@
+package org.imgt.asteroides;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VistaJuego extends View {
+    // //// ASTEROIDES //////
+    private List<Grafico> asteroides; // Lista con los Asteroides
+    private int numAsteroides = 5; // Número inicial de asteroides
+    private int numFragmentos = 3; // Fragmentos en que se divide
+
+    public VistaJuego(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        Drawable drawableNave, drawableAsteroide, drawableMisil;
+        drawableAsteroide = //context.getResources().getDrawable(R.drawable.asteroide1);
+                            ContextCompat.getDrawable(context, R.drawable.asteroide1);
+
+        asteroides = new ArrayList<Grafico>();
+        for (int i = 0; i < numAsteroides; i++) {
+            Grafico asteroide = new Grafico(this, drawableAsteroide);
+            asteroide.setIncY(Math.random() * 4 - 2);
+            asteroide.setIncX(Math.random() * 4 - 2);
+            asteroide.setAngulo((int) (Math.random() * 360));
+            asteroide.setRotacion((int) (Math.random() * 8 - 4));
+            asteroides.add(asteroide);
+        }
+    }
+
+    @Override protected void onSizeChanged(int ancho, int alto,
+                                           int ancho_anter, int alto_anter) {
+        super.onSizeChanged(ancho, alto, ancho_anter, alto_anter);
+        // Una vez que conocemos nuestro ancho y alto.
+        for (Grafico asteroide: asteroides) {
+            asteroide.setCenX((int) (Math.random()*ancho));
+            asteroide.setCenY((int) (Math.random()*alto));
+        }
+    }
+
+    @Override protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        for (Grafico asteroide: asteroides) {
+            asteroide.dibujaGrafico(canvas);
+        }
+    }
+}
