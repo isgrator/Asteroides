34ade84cf62d8d687fa7147acf513256a7decc83
Isabel <isgrator@gmail.com>
2017-12-26 16:29:45 +0100

    Verificando acceso a la memoria externa (pág.391)

--- app/src/main/java/org/imgt/asteroides/AlmacenPuntuacionesFicheroExterno.java
+++ app/src/main/java/org/imgt/asteroides/AlmacenPuntuacionesFicheroExterno.java
@@ -3,8 +3,10 @@ package org.imgt.asteroides;
 import android.content.Context;
 import android.os.Environment;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
@@ -24,6 +26,15 @@ public class AlmacenPuntuacionesFicheroExterno implements AlmacenPuntuaciones {
     }
 
     public void guardarPuntuacion(int puntos, String nombre, long fecha){
+
+        //comprueba el estado de acceso a la memoria externa
+        String stadoSD = Environment.getExternalStorageState();
+        boolean extraible= Environment.isExternalStorageRemovable();
+        if(!stadoSD.equals(Environment.MEDIA_MOUNTED)){
+            Toast.makeText(context, context.getString(R.string.error_escritura), Toast.LENGTH_LONG).show();
+            return;
+        }
+
         try {
             FileOutputStream f = new FileOutputStream(FICHERO, true);
             String texto = puntos + " " + nombre + "\n";
@@ -35,7 +46,18 @@ public class AlmacenPuntuacionesFicheroExterno implements AlmacenPuntuaciones {
     }
 
     public List<String> listaPuntuaciones(int cantidad) {
+
         List<String> result = new ArrayList<String>();
+        //comprueba el estado de acceso a la memoria externa
+        String stadoSD = Environment.getExternalStorageState();
+
+        if (!stadoSD.equals(Environment.MEDIA_MOUNTED) &&
+                !stadoSD.equals(Environment.MEDIA_MOUNTED_READ_ONLY)) {
+            Toast.makeText(context, context.getString(R.string.error_lectura),
+                    Toast.LENGTH_LONG).show();
+            return result;
+        }
+
         try {
             FileInputStream f = new FileInputStream(FICHERO);
             BufferedReader entrada = new BufferedReader(
@@ -55,4 +77,6 @@ public class AlmacenPuntuacionesFicheroExterno implements AlmacenPuntuaciones {
         }
         return result;
     }
+
+    //Método para verificar el acceso a la memoria externa
 }
