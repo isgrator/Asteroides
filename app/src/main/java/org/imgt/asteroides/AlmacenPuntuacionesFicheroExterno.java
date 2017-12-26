8194a9cff7b23d2f6b48c98be551b0eb69a875d5
Isabel <isgrator@gmail.com>
2017-12-26 02:15:43 +0100

    Configurar almacenamiento de puntuaciones desde preferencias (9.4.1) Almacenando puntuaciones en la memoria externa (9.4.2)

---
+++ app/src/main/java/org/imgt/asteroides/AlmacenPuntuacionesFicheroExterno.java
@@ -0,0 +1,58 @@
+package org.imgt.asteroides;
+
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Isabel María on 25/12/2017.
+ */
+
+public class AlmacenPuntuacionesFicheroExterno implements AlmacenPuntuaciones {
+    private static String FICHERO = Environment.getExternalStorageDirectory() + "/puntuaciones.txt";
+    private Context context;
+
+    public AlmacenPuntuacionesFicheroExterno(Context context) {
+        this.context = context;
+    }
+
+    public void guardarPuntuacion(int puntos, String nombre, long fecha){
+        try {
+            FileOutputStream f = new FileOutputStream(FICHERO, true);
+            String texto = puntos + " " + nombre + "\n";
+            f.write(texto.getBytes());
+            f.close();
+        } catch (Exception e) {
+            Log.e("Asteroides", e.getMessage(), e);
+        }
+    }
+
+    public List<String> listaPuntuaciones(int cantidad) {
+        List<String> result = new ArrayList<String>();
+        try {
+            FileInputStream f = new FileInputStream(FICHERO);
+            BufferedReader entrada = new BufferedReader(
+                    new InputStreamReader(f));
+            int n = 0;
+            String linea;
+            do {
+                linea = entrada.readLine();
+                if (linea != null) {
+                    result.add(linea);
+                    n++;
+                }
+            } while (n < cantidad && linea != null);
+            f.close();
+        } catch (Exception e) {
+            Log.e("Asteroides", e.getMessage(), e);
+        }
+        return result;
+    }
+}
